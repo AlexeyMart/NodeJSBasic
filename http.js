7794b2f0c1bb852f902124b2910506b2c66b32a5
Alex Mart <alexey.martyshev@mail.ru>
2024-06-24 16:13:30 +0300

    request.url, request.method

--- http.js
+++ http.js
@@ -1,28 +1,44 @@
 const http = require("http");
 const fs = require("fs");
 
-const server = http.createServer((_request, response) => {
-  //   TEXT response
-  //   response.writeHead(200, { "Content-Type": "text/plain" });
-  //   response.end("Hello world!");
+const server = http.createServer((request, response) => {
+  //   const method = request.method // GET POST PUT DELETE
+  const url = request.url;
+
+  // TEXT response
+  if (url === "/") {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello world!");
+    return;
+  }
 
   // JSON response
-  //   response.writeHead(200, { "Content-Type": "application/json" });
-  //   response.end(
-  //     JSON.stringify({
-  //       id: 10,
-  //       email: "example@example.com",
-  //     })
-  //   );
+  if (url === "/api") {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(
+      JSON.stringify({
+        id: 10,
+        email: "example@example.com",
+      })
+    );
+    return;
+  }
 
-  //   HTML Response
-  response.writeHead(200, { "Content-Type": "text/html" });
-  // for large files
-  // fs.createReadStream(__dirname + "/index.html").pipe(response);
-  const userName = "Some user name";
-  let html = fs.readFileSync(__dirname + "/index.html", "utf-8");
-  html = html.replace("{{userName}}", userName);
-  response.end(html);
+  // HTML Response
+  if (url === "/about") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    // for large files
+    // fs.createReadStream(__dirname + "/index.html").pipe(response);
+    const userName = "Some user name";
+    let html = fs.readFileSync(__dirname + "/index.html", "utf-8");
+    html = html.replace("{{userName}}", userName);
+    response.end(html);
+    return;
+  }
+
+  // Not found
+  response.writeHead(404);
+  response.end("Page not found");
 });
 
 server.listen(3000, () => {
